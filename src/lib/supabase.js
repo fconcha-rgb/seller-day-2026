a8cbf8ddcc723b4841df34e3533a8ccb31f4b842
fconcha@falabella.cl <fconcha@falabella.cl>
2026-03-25 09:04:46 -0300

    app completa

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -4,6 +4,6 @@ import { createClient } from '@supabase/supabase-js'
 // Ve a tu notepad y copia los valores del Paso A3
 
 const SUPABASE_URL = 'https://nhyedrvkxhyyoqxgcmrg.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_J5bnTFDBgoLu19XPKuZisQ_Q-Qj-kkM'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oeWVkcnZreGh5eW9xeGdjbXJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzODU2NDgsImV4cCI6MjA4OTk2MTY0OH0.rA5fkCJHUdQtehSs0iAAiPI2J-D8TLNd-8MOw6K2a-4'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
